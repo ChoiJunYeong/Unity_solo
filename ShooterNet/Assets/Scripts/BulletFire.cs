404d8c8fcd4363524d9b020558d659b7f51891c3
ChoiJunYeong <givenzero0@gmail.com>
2017-06-08 19:36:16 +0900

    make network game follow book 절대강좌 유니티5

---
+++ ShooterNet/Assets/Scripts/BulletFire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour {
+    public float lifeTime = 5.0f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = transform.forward * 10.0f;
+        Destroy(gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ShooterNet/Assets/Scripts/MoveCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour {
+    private Transform tr;
+    private CharacterController controlloer;
+    private float h = 0.0f;
+    private float v = 0.0f;
+    public float movSpeed = 5.0f;
+    public float rotSpeed = 50.0f;
+    private Vector3 movDir = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        this.enabled = GetComponent<NetworkView>().isMine;
+        tr = GetComponent<Transform>();
+        controlloer = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        tr.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime);
+        movDir = tr.forward * v + tr.right * h;
+        movDir.y -= 20f * Time.deltaTime;
+        controlloer.Move(movDir * movSpeed * Time.deltaTime);
+	}
+}
